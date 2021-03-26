2f47a159fcb5a57ee1443780577076722b3e66c4
Dwight Smith <dwightsmth@gmail.com>
2021-03-25 22:24:51 -0400

    initial commit

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ProfileRepository profileRepository) {
+        return args -> {
+
+            // Look into Faker object
+            Profile jessie = new Profile(
+                    "Jessie",
+                    "Warnock",
+                    22,
+                    "M",
+                    "Accountant"
+            );
+
+            profileRepository.save(jessie);
+        };
     }
 
 }
---
+++ src/main/java/com/example/demo/Profile.java
@@ -0,0 +1,124 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity(name = "Profile")
+public class Profile {
+
+    @Id
+    @SequenceGenerator(
+            name = "profile_sequence",
+            sequenceName = "profile_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "profile_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private int age;
+
+    @Column(
+            name = "gender"
+    )
+    private String gender;
+
+    @Column(
+            name = "occupation"
+    )
+    private String occupation;
+
+    public Profile() {
+    }
+
+    public Profile(String firstName, String lastName, int age, String gender, String occupation) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                ", id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", occupation='" + occupation + '\'' +
+                '}';
+    }
+}
